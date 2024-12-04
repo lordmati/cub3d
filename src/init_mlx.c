c2b8746d6c0f99558a3fdad9aac72c4fefd573c0
Matias Eduardo Isaguirre Escobar <misaguir@c1r5s5.42malaga.com>
2024-12-04 20:55:17 +0100

    toda la informacion del rayo ya esta

--- src/init_mlx.c
+++ src/init_mlx.c
@@ -1,5 +1,79 @@
 #include "../Include/cub3D.h"
 
+static void wall_finder(t_ray *ray, t_cub *game)
+{
+	while(1)
+	{
+		if (ray->sideDistX < ray->sideDistY)
+		{
+			ray->sideDistX += ray->deltaDistX;
+			ray->mapX += ray->stepX;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->sideDistY += ray->deltaDistY;
+			ray->mapY += ray->stepY;
+			ray->side = 1;
+		}
+		if (game->map[ray->mapY][ray->mapX] == '1'
+			|| game->map[ray->mapY][ray->mapX] == '\n'
+			|| game->map[ray->mapY][ray->mapX] == '\0'
+			|| game->map[ray->mapY][ray->mapX] == ' ')
+			break;
+	}
+}
+
+static void ray_steps(t_ray *ray, t_cub *game)
+{
+	if (ray->cos < 0)
+	{
+		ray->stepX = -1;
+		ray->sideDistX = (game->x - ray->mapX) * ray->deltaDistX;
+	}
+	else
+	{
+		ray->stepX = 1;
+		ray->sideDistX = (ray->mapX + 1.0 -game->x) * ray->deltaDistX;
+	}
+	if (ray->sin < 0)
+	{
+		ray->stepY = -1;
+		ray->sideDistY = (game->y - ray->mapY) * ray->deltaDistY;
+	}
+	else
+	{
+		ray->stepY = 1;
+		ray->sideDistY = (ray->mapY + 1.0 - game->y) * ray->deltaDistY;
+	}
+}
+
+static void dda_algorithm(t_ray *ray, t_cub *game, int i)
+{
+	int start;
+	int end;
+	ray->mapX = (int)game->x;
+	ray->mapY = (int)game->y;
+	ray->deltaDistX = fabs(1 / ray->cos);
+	ray->deltaDistY = fabs(1 / ray->sin);
+	ray_steps(ray, game);
+	wall_finder(ray, game);
+	if (ray->side == 0)
+		ray->distance = (ray->mapX - game->x + (1 - ray->stepX)  / 2) / ray->cos;
+	else
+		 ray->distance = (ray->mapY - game->y + (1 - ray->stepY) / 2) / ray->sin;
+	ray->distance = ray->distance * cos(ray->ang - game->radian_view);
+	ray->x_r = game->x + ray->distance * ray->cos;
+    ray->y_r = game->y + ray->distance * ray->sin;
+	start =MAP_HEIGHT/2 - (MAP_HEIGHT/(2 * ray->distance));
+	end =MAP_HEIGHT/2 + (MAP_HEIGHT/(2 * ray->distance));
+	while(start<=end)
+	{
+		mlx_put_pixel(game->cub_img,i,start,rgb(39,168,54,255));
+		start++;
+	}
+}
+
 static void ray_casting(t_ray *ray,t_cub *game)
 {
 	int i = 0;
@@ -10,12 +84,12 @@ static void ray_casting(t_ray *ray,t_cub *game)
 	radian_fov = (FOV * M_PI) / 180;
 	ang = (FOV / MAP_WIDTH) * M_PI / 180;
 	start = game->radian_view + radian_fov / 2;
-	while(i < 3)
+	while(i < MAP_WIDTH)
 	{
-		ray[i].ang = start + (ang * i);
+		ray[i].ang = start - (ang * i);
 		ray[i].sin = sin(ray[i].ang);
 		ray[i].cos = cos(ray[i].ang);
-		printf("%f\n",ray[i].ang);
+		dda_algorithm(&ray[i], game, i);
 		i++;
 	}
 }
@@ -52,5 +126,4 @@ void init_mlx(t_cub *game)
 	mlx_image_to_window(game->mlx, game->cub_img, 0, 0);
 	ray_casting(ray,game);
 	mlx_loop(game->mlx);
-
 }
--- src/set_view.c
+++ src/set_view.c
@@ -3,11 +3,11 @@
 double set_view(t_cub *game)
 {
 	if (game->player_view == 'E')
-		return(0);
-	else if(game->player_view == 'N')
-		return(M_PI_2);
-	else if(game->player_view == 'W')
 		return(M_PI);
+	else if(game->player_view == 'N')
+		return(M_PI_2 + M_PI);
+	else if(game->player_view == 'W')
+		return(0);
 	else
-		return(M_PI + M_PI_2);
+		return(M_PI_2);
 }
